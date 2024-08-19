4cfc478f753130a616dc226b33a9225778b6691e
fabritac <71648329+fabritac@users.noreply.github.com>
2024-08-19 15:04:55 -0300

    Update init.el

--- init.el
+++ init.el
@@ -1,39 +1,65 @@
-;;; Appearance
-(tool-bar-mode 0)
-(menu-bar-mode 0)
-(scroll-bar-mode 0)
-(column-number-mode 1)
-(show-paren-mode 1)
-(setq display-line-numbers-type 'relative) 
-(global-display-line-numbers-mode)
+; Basic config
+(setq package-enable-at-startup nil
+      inhibit-startup-message   t
+      frame-resize-pixelwise    t  ; fine resize
+      package-native-compile    t) ; native compile packages
 
-;;; Melpa
+(scroll-bar-mode -1)
+(tooltip-mode -1)
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+
+; Visual configuration
+(setq visible-bell t)
+; Change cursor  
+(setq x-stretch-cursor t)
+; Column number 
+(column-number-mode)
+; Set font and size
+(set-face-attribute 'default nil :font "Consolas" :height 140)
+;(set-face-attribute 'default nil :font "Lucida Console" :height 140)
+;(set-face-attribute 'default nil :font "Anonymous Pro" :height 140)
+; Set theme
+;(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
+(load-theme 'wombat t)
+
+; Melpa
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
-;; and `package-pinned-packages`. Most users will not need or want to do this.
-;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 (package-initialize)
 
-;;; Backup and autosave
+; Backup and autosave
 (setq make-backup-files nil)
 (setq auto-save-default nil)
 
-;;; Misc
-;; Remember and restore the last cursor location of opened files
-(save-place-mode 1)
-;; Move customization variables to a separate file and load it
+; Misc
+; Remember and restore the last cursor location of opened files
+(save-place-mode 1) 
+; Move customization variables to a separate file and load it
 (setq custom-file (locate-user-emacs-file "custom-vars.el"))
 (load custom-file 'noerror 'nomessage)
-;; Revert buffers when the underlying file has changed
+; Revert buffers when the underlying file has changed
 (global-auto-revert-mode 1)
-;; Revert Dired and other buffers
+; Revert Dired and other buffers
 (setq global-auto-revert-non-file-buffers t)
-
-;;; Evil mode
-;; Download Evil
-(unless (package-installed-p 'evil)
-  (package-install 'evil))
-;; Enable Evil
-(require 'evil)
-(evil-mode 1)
+; Make editing text files more intuitively
+(setq-default sentence-end-double-space nil)
+; Jump words like camelCase better
+(global-subword-mode 1)
+; Stop the cursor from changing half of the screen when scrolling
+(setq scroll-conservatively 1000)
+; Disable tabs for indent 
+(setq-default indent-tabs-mode nil)
+; Delete files in dired moves things to trash
+(setq delete-by-moving-to-trash t)
+; Dont show message in scratch buffer
+(setq-default initial-scratch-message nil)
+; Auto save and better undo memory
+(setq undo-limit        100000000
+      auto-save-default t)
+; take new window space from all other windows
+(setq window-combination-resize t)
+; Short answers
+(setopt use-short-answers t)
+; Make Ctrl+Enter save file
+;(global-set-key [C-return] 'save-buffer)
