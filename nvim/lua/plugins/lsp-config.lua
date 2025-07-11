94618531c6a06cdb02942185f49bcf5099400f9a
fabritac <71648329+fabritac@users.noreply.github.com>
2025-07-11 12:54:31 -0300

    update

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -1,10 +1,12 @@
 return {
     {
         "neovim/nvim-lspconfig",
+        dependencies = { 'saghen/blink.cmp' },
         config = function()
             local lspconfig = require("lspconfig")
-            lspconfig.lua_ls.setup {}
-            lspconfig.clangd.setup {}
+            local capabilities = require('blink.cmp').get_lsp_capabilities()
+            lspconfig.lua_ls.setup { capabilities = capabilities }
+            lspconfig.clangd.setup { capabilities = capabilities }
         end,
     }
 }
